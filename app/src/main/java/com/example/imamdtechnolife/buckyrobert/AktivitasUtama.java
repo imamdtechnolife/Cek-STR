67ef28029bfbc3a25d1b71ccf431408fbcc59da8
imamdtechnolife <imamdtechnolife@gmail.com>
2018-02-12 00:11:33 +0800

    first commit

---
+++ app/src/main/java/com/example/imamdtechnolife/buckyrobert/AktivitasUtama.java
@@ -0,0 +1,13 @@
+package com.example.imamdtechnolife.buckyrobert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AktivitasUtama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktivitas_utama);
+    }
+}
